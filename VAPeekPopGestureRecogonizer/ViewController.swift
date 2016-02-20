37d8ba1c9de550b8ec942dcac6aa525ed9321aa7
Vaibhav Aggarwal <vaggarwal@myfitnesspal.com>
2016-02-20 00:05:10 -0800

    Initial Commit

---
+++ VAPeekPopGestureRecogonizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VAPeekPopGestureRecogonizer
+//
+//  Created by Vaibhav Aggarwal on 2/20/16.
+//  Copyright © 2016 Vaibhav Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
